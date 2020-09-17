e3debb0d61acc528e1a6eee81da5b4303464f29c
Ubuntu <ubuntu@ip-172-31-22-56.ap-northeast-1.compute.internal>
2020-09-17 01:37:12 +0000

    complete setting Devise

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -4,4 +4,5 @@ class IndexController < ApplicationController
 
   def terms
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # , skip: :all
+  
+  # devise_scope :user do
+  #   get 'signup', to: 'devise/registrations#new'
+  #   post 'signup', to: 'devise/registrations#create'
+  #   get 'login', to: 'users/sessions#new'
+  #   post 'login', to: 'users/sessions#create'
+  #   delete 'logout', to: 'devise/sessions#destroy'
+  #   get 'passwordedit', to: 'devise/passwords#edit'
+  #   put 'passwordedit', to: 'devise/passwords#update'
+  # end
+  
   root 'index#home'
   get    '/terms',  to: 'index#terms'
+  resources :users, only: [:show]
 end
