3c3819f4ddfa91814e75bb8eac455cfe513c25d0
Ubuntu <ubuntu@ip-172-31-22-56.ap-northeast-1.compute.internal>
2020-09-28 16:05:09 +0000

    notice update 完了

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,23 +1,17 @@
 module NotificationsHelper
+  
   def notification_form(notification)
-    #通知を送ってきたユーザーを取得
-    @visitor = notification.visitor
-    #コメントの内容を通知に表示する
-    @comment = nil
-    @visitor_comment = notification.comment_id
-    # notification.actionがfollowかlikeかcommentかで処理を変える
-    case notification.action
-    when 'follow'
-      #aタグで通知を作成したユーザーshowのリンクを作成
-      tag.a(notification.visitor.name, href: user_path(@visitor)) + 'is following you'
-    when 'like'
-      tag.a(notification.visitor.name, href: user_path(@visitor)) + 'liked' + tag.a('your post', href: post_path(notification.post_id))
-    when 'comment' then
-      #コメントの内容と投稿のタイトルを取得　      
-      @comment = Comment.find_by(id: @visitor_comment)
-      @comment_content =@comment.content
-      @post_title =@comment.micropost.title
-      tag.a(@visitor.name, href: user_path(@visitor)) + 'commented' + tag.a("#{@post_title}", href: post_path(notification.post_id))
-    end
+  @comment=nil
+  visitor=link_to notification.visitor.name, notification.visitor, style:"font-weight: bold;"
+  your_post=link_to 'あなたの投稿', notification.post, style:"font-weight: bold;", remote: true
+  case notification.action
+    when "follow" then
+      "#{visitor}があなたをフォローしました"
+    when "like" then
+      "#{visitor}が#{your_post}にいいね！しました"
+    when "comment" then
+      @comment=Comment.find_by(id:notification.comment_id)&.content
+      "#{visitor}が#{your_post}にコメントしました"
+  end
   end
 end
